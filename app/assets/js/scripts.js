3d10e2328bd572ea96bf161555abf17287e70f97
Henrique Maio <henriquemaio@Henriques-MacBook.local>
2015-10-02 11:27:27 -0300

    Adicionada a função de upload da foto do perfil

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,17 @@
+function getPhoto() {
+    document.getElementById("upload").click();
+}
+
+function readURL(input, id) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#'+id)
+        .attr('src', e.target.result)
+        ;
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
